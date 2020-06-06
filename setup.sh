ccd68e47775aadffd4ff4832b819b7d6b1463663
Caduri Katzav <cadurik@gmail.com>
2020-06-06 21:51:51 +0300

    fix - script

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ else    printf '%s\n' "Install Command Line Tools..."
         osascript -e 'tell application "System Events"' -e 'tell process "Install Command Line Developer Tools"' -e 'keystroke return' -e 'click button "Agree" of window "License Agreement"' -e 'end tell' -e 'end tell'
 
 # Install Homebrew
-if      type -P brew >/dev/null 2>&1
+if      type brew >/dev/null 2>&1
 then    printf '%s\n' "Installing homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else    printf '%s\n' "Homebrew is installed, updating..."
@@ -16,7 +16,7 @@ else    printf '%s\n' "Homebrew is installed, updating..."
 fi
 
 # Install Python3
-if      type -P python3 >/dev/null 2>&1
+if      type python3 >/dev/null 2>&1
 then    printf '%s\n' "Installing python..."
         brew install python
 else    printf '%s\n' "Python is installed"
